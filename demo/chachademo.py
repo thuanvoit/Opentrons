9cbeb45192613c36a00a9903ae62bec7d9aa4629
transplant <aileen.johnson@emory.edu>
2021-10-08 12:41:24 -0400

    tilt up L5

--- demo/chachademo.py
+++ demo/chachademo.py
@@ -14,7 +14,7 @@ rows = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P']
 
 blocking_position = {
     'slide1': {'rows': ['F', 'G'], 'cols': ['2', '3']},
-    'slide2': {'rows': ['F', 'G'], 'cols': ['4', '10']},
+    'slide2': {'rows': ['F', 'G'], 'cols': ['9', '10']},
     'slide3': {'rows': ['F', 'G'], 'cols': ['16', '17']},
     'slide4': {'rows': ['F', 'G'], 'cols': ['23', '24']},
     }
@@ -22,7 +22,8 @@ blocking_position = {
 def chacha_wash(protocol, pipette, chacha, n):
     for i in range(n):
         pipette.move_to(chacha['A6'].top(20))
-        pipette.move_to(chacha['A6'].top(-10), speed=100)
+        pipette.move_to(chacha['A6'].top(-5), speed=20)
+        pipette.move_to(chacha['A6'].top(-10), speed=150)
         protocol.delay(seconds=5)
         pipette.move_to(chacha['L6'].top(20))
         pipette.move_to(chacha['L6'].top(), speed=20)
@@ -31,7 +32,7 @@ def chacha_blocking(protocol, pipette, chacha, from_tupe, volume, to_cols, to_ro
     pipette.aspirate(volume, from_tupe)
     for col in to_cols:
         for row in to_rows:
-            pipette.dispense(volume/4, location=chacha[row[0]+col[0]].top(), rate=0.5)
+            pipette.dispense(volume/4, location=chacha[row+col].top(), rate=0.5)
     pipette.blow_out(location=chacha[to_rows[-1]+to_cols[-1]].top())
    
 
@@ -56,6 +57,8 @@ def run(protocol: protocol_api.ProtocolContext):
     # left_pipette.blow_out(tuberack['A1'].top(-10))
 
     chacha_blocking(protocol, left_pipette, chacha, h2o_tube, 300, blocking_position['slide1']['cols'], blocking_position['slide1']['rows'])
+    chacha_blocking(protocol, left_pipette, chacha, h2o_tube, 300, blocking_position['slide2']['cols'], blocking_position['slide2']['rows'])
+    chacha_blocking(protocol, left_pipette, chacha, h2o_tube, 300, blocking_position['slide3']['cols'], blocking_position['slide3']['rows'])
     chacha_wash(protocol, left_pipette, chacha, 3)
 
     left_pipette.return_tip()
